b33145c2c1084751fe4df824195b151d88431a0d
Tobias-Ternent <tobias@ebi.ac.uk>
2017-03-16 09:40:25 +0000

    Added unit test for registering a DOI.

--- src/test/java/uk/ac/ebi/pride/px/doi/DemoApplicationTests.java
+++ src/test/java/uk/ac/ebi/pride/px/doi/DemoApplicationTests.java
@@ -2,16 +2,48 @@ package uk.ac.ebi.pride.px.doi;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import uk.ac.ebi.pride.px.doi.model.DoiRegistration;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static uk.ac.ebi.pride.px.doi.model.DoiRegistrationStatus.SUCCESS;
+
+/**
+ * This class tests the PX DOI WS to register a DOI for a project.
+ */
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class DemoApplicationTests {
 
-	@Test
-	public void contextLoads() {
-		//todo
-	}
+  @Value("${pride.username.property}")
+  private String prideUsername;
+  @Value("${pride.password.property}")
+  private String pridePassword;
 
+  @Autowired
+  private TestRestTemplate restTemplate;
+
+  /**
+   * This method does a test POST to register a DOI, to the test environment.
+   * A DOI is not actually registered, but a dummy SUCCESS message is returned.
+   */
+  @Test
+  public void testRegisterDoi() {
+    ResponseEntity<DoiRegistration> entity = restTemplate
+        .withBasicAuth(prideUsername, pridePassword)
+        .postForEntity("/registration/test123/mode/test"
+            , ""
+            , DoiRegistration.class
+        );
+    assertThat(SUCCESS.equals(entity.getBody().getStatus()));
+    assertThat("SUCCESS".equals(entity.getBody().getMessage()));
+  }
 }
